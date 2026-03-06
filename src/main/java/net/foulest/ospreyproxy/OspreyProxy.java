152c633ce24c401090c5efca5cfc9e8cc590a13c
Foulest <43710301+Foulest@users.noreply.github.com>
2026-03-06 03:46:07 -0800

    Excluded errors in OspreyProxy

--- src/main/java/net/foulest/ospreyproxy/OspreyProxy.java
+++ src/main/java/net/foulest/ospreyproxy/OspreyProxy.java
@@ -2,10 +2,16 @@ package net.foulest.ospreyproxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
 public class OspreyProxy {
 
+    /**
+     * Main method to start the Spring Boot application.
+     *
+     * @param args - Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         SpringApplication.run(OspreyProxy.class, args);
     }
